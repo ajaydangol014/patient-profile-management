f21050e377fa451f02e3135cfda6052be3b90c2d
ajaydangol014 <ajaydangol014@gmail.com>
2023-05-01 23:18:16 +0545

    LFUNIOLD-227: Add sorting in table header

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import { useTable, useSortBy } from "react-table";
 const Table = ({ columns, data }) => {
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns, data }, useSortBy);
+  console.log(useTable({ columns, data }));
 
   return (
     <table {...getTableProps()}>
