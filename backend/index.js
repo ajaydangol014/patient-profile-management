2619bfea0cc93288f852fd979b852c1c2aa7d2a3
ajaydangol014 <ajaydangol014@gmail.com>
2023-05-08 19:12:03 +0545

    LFUNIOLD-227: Add router constant in backen

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const routers = require("./routes/api.routes");
 // const db = require("./database/db"); // by using the pool we can run queries with postgres
 
 //middleware - any time you're gonna make middle ware we have to use .use();
@@ -8,7 +9,7 @@ app.use(cors());
 app.use(express.json()); // allowing us to give us access to request that body and we can get json data.
 
 // routes for application
-app.use("/api", require("./routes/api.routes")); //which is mapped to all the request coming to api
+app.use("/api", routers); //which is mapped to all the request coming to api
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
